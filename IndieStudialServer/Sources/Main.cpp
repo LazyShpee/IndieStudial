b3cf52d13589608996d38af3b3239dc6f2fbb97f
LazyShpee <comemureravaud@gmail.com>
2016-06-05 16:55:15 +0200

    .

---
+++ IndieStudialServer/Sources/Main.cpp
@@ -0,0 +1,15 @@
+#include "TCPInterface.h"
+#include "ConsoleServer.h"
+#include "RakNetCommandParser.h"
+#include "TelnetTransport.h"
+
+#include "RakPeerInterface.h"
+#include "LogCommandParser.h"
+#include "GetTime.h"
+#include "RakNetTransport2.h"
+#include "LinuxStrings.h"
+#include <stdio.h>
+
+int main(int c, char *v[]) {
+
+}
