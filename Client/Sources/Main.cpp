66ea45ba60ad078e6195717e68a0c26baa37cebd
dihamon <brain.entity@gmail.com>
2016-06-09 02:37:01 +0200

    omg windaube

---
+++ Client/Sources/Main.cpp
@@ -0,0 +1,46 @@
+#include "Loop.hpp"
+#include "Constants.hpp"
+
+#if defined (_IRR_WINDOWS_) && ! defined (MSYS)
+# pragma comment(lib, "Irrlicht.lib")
+//# pragma comment(linker, "/subsystem:windows /ENTRY:mainCRTStartup")
+#endif
+
+/* 
+ * bonjour je suis le main
+ * pour l'instant je contient l'init de la lib et la boucle de jeu
+ */
+int main()
+{
+  core::device_t device;
+  core::Receiver receiver;
+  core::ILoop *loop[2];
+  int ret;
+
+  /* lib and device init */
+  device.ptr = irr::createDevice(ivideo::EDT_NULL,
+				 icore::dimension2d<irr::u32>(1280, 720), 16,
+				 false, true, false, &receiver);
+  if (!device.ptr)
+    return (ERROR_CODE);
+  device.driver = device.ptr->getVideoDriver();
+  device.smgr = device.ptr->getSceneManager();
+  device.guienv = device.ptr->getGUIEnvironment();
+
+  rayzal::Peer peer("localhost");
+  rayzal::ListenerThread listener(device.smgr, &peer);
+  loop[0] = new core::GameLoop(&device, &peer);
+  // loop[1] = new core::MenuLoop(&device);
+  if (loop[0]->init())
+    return (ERROR_CODE);
+  // if (loop[1]->init())
+  //   return (ERROR_CODE);
+
+  ret = 0;
+  while (device.ptr->run())
+    ret = loop[ret]->loop();
+  device.ptr->drop();
+  // delete loop[1];
+  delete loop[0];
+  return (OK_CODE);
+}
